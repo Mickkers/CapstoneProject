82a1f2dff5c83ab75ab8718a4ba5a9f235615bdf
Mickkers <90241018+Mickkers@users.noreply.github.com>
2024-01-18 13:03:34 +0700

    Workshop Updates

--- Assets/Scripts/WorldObjects/Podium.cs
+++ Assets/Scripts/WorldObjects/Podium.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Podium : Interactible
 {
-    
+    [SerializeField] private 
 
     // Start is called before the first frame update
     void Start()
